d77c4d4dbf0ba7e156b5add779b97d980b78be07
Isack <ituwei21@gmail.com>
2021-11-05 17:14:42 +0300

    fixed a bug

--- client/src/index.js
+++ client/src/index.js
@@ -5,11 +5,26 @@ import Home from './components/Home';
 import * as serviceWorker from './serviceWorker';
 import history from './History';
 import {Route,Router,Switch} from 'react-router-dom'; 
+import AddCandidate from './components/AddCandidate';
+import CandidateDetails from './components/CandidateDetails';
+import RequestVoter from './components/RequestVoter';
+import VerifyVoter from './components/VerifyVoter';
+import Vote from './components/Vote';
+import Result from './components/Result';
+import Admin from './components/Admin';
+
 
 ReactDOM.render(
 <Router history={history}>
     <Switch>
         <Route exact path='/' component={Home}/>
+        <Route exact path='/AddCandidate' component={AddCandidate} />
+        <Route exact path='/CandidateDetails' component={CandidateDetails} />
+        <Route exact path='/RequestVoter' component={RequestVoter} />
+        <Route exact path='/VerifyVoter' component={VerifyVoter} />
+        <Route exact path='/Vote' component={Vote} />
+        <Route exact path='/Result' component={Result} />
+        <Route exact path='/Admin' component={Admin} />
     </Switch>
 
 </Router>, document.getElementById('root'));
